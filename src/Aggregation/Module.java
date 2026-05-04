2f3beeda978dee4b3f280deb5fb5fc3fdd0e9e91
vishwageethanjana <geeth.me67@gmail.com>
2026-05-04 13:40:58 +0530

    ecommerce 04/05/2026

---
+++ src/Aggregation/Module.java
@@ -0,0 +1,10 @@
+package Aggregation;
+
+import java.util.List;
+
+public class Module {
+
+    String name;
+    int age;
+    List<Module> modules;   // Aggregation - bcz lecture can exist without a module
+}
