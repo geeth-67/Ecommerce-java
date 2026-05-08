83c66ba17302f14f176214e885ea8a8b1cbb64c0
vishwageethanjana <geeth.me67@gmail.com>
2026-05-08 13:11:58 +0530

    ecommerce 04/05/2026

--- src/GroceryProduct.java
+++ src/GroceryProduct.java
@@ -1,4 +1,4 @@
-public class GroceryProduct extends Product {
+public class GroceryProduct extends Product implements Discountable{
 
     private String expiryDate;
 
@@ -12,5 +12,21 @@ public class GroceryProduct extends Product {
     @Override
     String getCategory() {
         return "Grocery";
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public double getDiscountedPrice(int discountPercentage) {
+
+        double discount  = getProdPrice() * (discountPercentage / 100.00);
+
+        return getProdPrice() - discount;
     }
 }
